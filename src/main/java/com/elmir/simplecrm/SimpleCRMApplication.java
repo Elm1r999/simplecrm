4c266882051bfd9ff9b71d471fd57b6c6a21c0d0
Elmir Ahadov <elmir.ahadov@yahoo.com>
2020-06-16 23:48:03 +0400

    Initial commit

---
+++ src/main/java/com/elmir/simplecrm/SimpleCRMApplication.java
@@ -0,0 +1,14 @@
+package com.elmir.simplecrm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author ELMIR
+ */
+@SpringBootApplication
+public class SimpleCRMApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleCRMApplication.class, args);
+	}
+}
---
+++ src/test/java/com/elmir/simplecrm/SimpleCRMApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elmir.simplecrm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleCRMApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
